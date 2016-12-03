7d2b777bd1afdca99bfeed066896034149017919
TeppieC <teppie.c@hotmail.com>
2016-12-03 03:21:45 -0700

    finished template

--- learning.py
+++ learning.py
@@ -5,8 +5,22 @@ from pylab import *  # includes numpy
 numRuns = 1
 # numTiles: 4*4*9
 n = numTiles * 3 # number of components
-phi = [-1]*numTilings  # initialize the list of tileindices
 gamma = 1
+
+def createFeature(phi, indices):
+    #indices = [action*numTiles+index for index in tileIndices]
+    for index in indices:
+        phi[index] = 1
+    return phi
+
+def product(theta, phi, indices):
+    result = 0
+    for index in indices:
+        if not phi[index]==1:
+            print('Error. Not correspond phi')
+        else:
+            result += theta
+    return result
 
 def learn(alpha=.1/numTilings, epsilon=0, numEpisodes=200):
     theta1 = -0.001*rand(n)
@@ -14,17 +28,89 @@ def learn(alpha=.1/numTilings, epsilon=0, numEpisodes=200):
     returnSum = 0.0
     for episodeNum in range(numEpisodes):
         G = 0
-        S = mountaincar.init()
+        state = mountaincar.init() # S[0] is the position, S[1] is the velocity
+        start = True
 
         while True:
 
-            tilecode(S[0], S[1], phi)
+            q1 = [0]*3 # for each possible actions, each has a q value
+            q2 = [0]*3
+            phi = [0]*n # initialize the list of features ø
+            tileIndices = [-1]*numTilings
+            tilecode(state[0], state[1], tileIndices)
 
-            if np.random.randint(0,2): # with 0.5 probability
-                theta1[state][action]=theta1[state][action]+alpha*(reward+gamma*theta2[nextState][argmax(theta1[nextState])]-theta1[state][action])
-            else:  # with 0.5 probability
-                theta2[state][action]=theta2[state][action]+alpha*(reward+gamma*theta1[nextState][argmax(theta2[nextState])]-theta2[state][action])
-        
+            # choose action, from a epsilon greedy
+            num=np.random.random()
+            if (num>=epsilon):
+                for action in range(0,3):
+                    # generate q value for each possible actions
+                    for index in tileIndices: # implementing the vector multiplication thetaT*phi
+                        q1[action] = q1[action] + theta1[action*numTiles+index]*1
+                        q2[action] = q2[action] + theta2[action*numTiles+index]*1
+                action = argmax(q1+q2) # choose the greedy action
+            else:
+                action = np.random.randint(0,3) # choose the stochastic action
+
+            # actually generate the features, based on the action
+            indices = [action*numTiles+index for index in tileIndices] # indicates which position in phi is 1
+            # generating features, based on the action chosen in this state
+            for index in indices:
+                phi[index] = 1 # phi vector is generated for this state-action pair
+
+            # sample the next state, reward
+            reward, nextState = mountaincar.sample(state, action)
+            G = G+reward
+
+            if nextState==None:
+                # terminal state
+                theta1 = theta1 + alpha*(reward-theta1)*phi
+                theta2 = theta2 + alpha*(reward-theta2)*phi
+                break
+            else:
+                # not terminal state
+                # need to compute phi for the next state
+                nextQ1 = [0]*3
+                nextQ2 = [0]*3
+                nextPhi = [0]*n
+                nextTileIndices = [-1]*numTilings
+                tilecode(nextState[0], nextState[1], nextTileIndices)
+
+                '''
+                # choose action, from a epsilon greedy
+                num=np.random.random()
+                if (num>=epsilon):
+                    for action in range(0,3):
+                        # generate q value for each possible actions
+                        for index in nextTileIndices: # implementing the vector multiplication thetaT*phi
+                            q1[action] = q1[action] + theta1[action*numTiles+index]*1
+                            q2[action] = q2[action] + theta2[action*numTiles+index]*1
+                    nextAction = argmax(q1+q2) # choose the greedy action
+                else:
+                    nextAction = np.random.randint(0,3) # choose the stochastic action
+                '''
+
+                # next action is always greedy
+                for action in range(0,3):
+                    # generate q value for each possible actions
+                    for index in nextTileIndices: # implementing the vector multiplication thetaT*phi
+                        q1[action] = q1[action] + theta1[action*numTiles+index]*1
+                        q2[action] = q2[action] + theta2[action*numTiles+index]*1
+                nextAction = argmax(q1+q2) # choose the greedy action
+
+                # indicates which position in phi is 1
+                nextIndices = [nextAction*numTiles+index for index in nextTileIndices]
+                # generating features, based on the action chosen in this state
+                for index in nextIndices:
+                    phi[index] = 1 # phi vector is generated for this state-action pair
+
+                if np.random.randint(0,2): # with 0.5 probability
+                    theta1 = theta1 + alpha*(reward + gamma*product(theta2, nextPhi, nextIndices) - theta1)*phi
+                else:  # with 0.5 probability
+                    theta2 = theta2 + alpha*(reward + gamma*product(theta1, nextPhi, nextIndices) - theta2)*hi
+
+            state = nextState
+            #phi = nextPhi
+
         print("Episode: ", episodeNum, "Steps:", step, "Return: ", G)
         returnSum = returnSum + G
     print("Average return:", returnSum / numEpisodes)
