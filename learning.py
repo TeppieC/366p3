4568868ef55640279ebd2bc98e5b394a061528ec
TeppieC <teppie.c@hotmail.com>
2016-12-03 00:29:54 -0700

    setup

--- learning.py
+++ learning.py
@@ -3,8 +3,10 @@ from Tilecoder import numTilings, numTiles, tilecode
 from pylab import *  # includes numpy
 
 numRuns = 1
+# numTiles: 4*4*9
 n = numTiles * 3 # number of components
-tileIndices = [-1]*numTilings  # initialize the list of tileindices
+phi = [-1]*numTilings  # initialize the list of tileindices
+gamma = 1
 
 def learn(alpha=.1/numTilings, epsilon=0, numEpisodes=200):
     theta1 = -0.001*rand(n)
@@ -15,8 +17,14 @@ def learn(alpha=.1/numTilings, epsilon=0, numEpisodes=200):
         S = mountaincar.init()
 
         while True:
-            tilecode(S[0], S[1], tileIndices)
-            
+
+            tilecode(S[0], S[1], phi)
+
+            if np.random.randint(0,2): # with 0.5 probability
+                theta1[state][action]=theta1[state][action]+alpha*(reward+gamma*theta2[nextState][argmax(theta1[nextState])]-theta1[state][action])
+            else:  # with 0.5 probability
+                theta2[state][action]=theta2[state][action]+alpha*(reward+gamma*theta1[nextState][argmax(theta2[nextState])]-theta2[state][action])
+        
         print("Episode: ", episodeNum, "Steps:", step, "Return: ", G)
         returnSum = returnSum + G
     print("Average return:", returnSum / numEpisodes)
