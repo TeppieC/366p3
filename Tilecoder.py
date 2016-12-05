7b1390562fbdce49c33642d078e8a360514862c4
Aries0331 <jinzhu@ualberta.ca>
2016-12-05 12:38:54 -0700

    update

--- Tilecoder.py
+++ Tilecoder.py
@@ -1,6 +1,6 @@
 numTilings = 4
 numTiles = numTilings*9*9
-    
+
 def tilecode(in1,in2,tileIndices):
     # write your tilecoder here (5 lines or so)
 	for i in range(numTilings):
@@ -8,8 +8,18 @@ def tilecode(in1,in2,tileIndices):
 		newY = int((in2+0.07+0.0175/4*i)/0.0175)
 		tileIndices[i] = newX+newY*9+81*i
 	return tileIndices
-    
+
 def printTileCoderIndices(in1,in2):
     tileIndices = [-1]*numTilings
     tilecode(in1,in2,tileIndices)
-    print('Tile indices for input (', in1, ',', in2,') are : ', tileIndices)
+    #print('Tile indices for input (', in1, ',', in2,') are : ', tileIndices)
+
+'''
+if __name__ == '__main__':
+    printTileCoderIndices(-1.2, -0.07)
+    printTileCoderIndices(-1.2, 0.07)
+    printTileCoderIndices(0.5, -0.07)
+    printTileCoderIndices(0.5, 0.07)
+    printTileCoderIndices(-0.35, 0.0)
+    printTileCoderIndices(0.0, 0.0)
+'''
